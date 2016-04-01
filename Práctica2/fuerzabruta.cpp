fefade6a5be1543c488415ae448d358fdec796af
joserejon <jose.rejon.santiago@gmail.com>
2016-04-01 12:53:28 +0200

    fuerza bruta bueno

--- Práctica2/fuerzabruta.cpp
+++ Práctica2/fuerzabruta.cpp
@@ -9,25 +9,14 @@ using namespace std;
 //#include <algorithm>
 #include <vector>
 
-//generador de ejemplor para el problema de mezcla de k vectores ordenados. Para obtener k vectores ordenados de forma creciente cada uno con n elementos, genera un vector de tama�o
-//k*n con todos los enteros entre 0 y kn-1 ordenados. Se lanzan entonces k iteraciones de un algoritmo de muestreo aleatorio de tama�o n para obtener los
-//k vectores. Est�n ordeados porque el algoritmo de muestreo mantiene el orden
 
-double uniforme() //Genera un n�mero uniformemente distribuido en el
-                  //intervalo [0,1) a partir de uno de los generadores
-                  //disponibles en C.
-{
- int t = rand();
- double f = ((double)RAND_MAX+1.0);
- return (double)t/f;
-}
 
 int main(int argc, char * argv[])
 {
 
   if (argc != 4)
     {
-      cerr << "Formato " << argv[0] << "<ruta_del_archivo>  <num_elem>  <num_vect>" << endl;
+      cerr << "Formato " << argv[0] << "<ruta_del_archivo_de_entrada>  <num_elem>  <num_vect>" << endl;
       return -1;
     }
 
---
+++ Práctica2/fuerzabruta_bueno.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <climits>
+#include <cassert>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+vector<int> combinarVectores(const vector<int> v1, const vector<int> v2){
+	int i = 0, j = 0;
+	vector<int> v_resultado;
+
+	//Meter números ordenados en el vector ordenado
+	while(i < v1.size() && j < v2.size()){
+		if(v1.at(i) < v2.at(j)){
+			v_resultado.push_back(v1.at(i));
+			i++;
+		}
+		else{
+			v_resultado.push_back(v2.at(j));
+			j++;
+		}
+	}
+
+	//Terminar de completar vector resultado
+	for(i; i < v1.size(); i++)
+		v_resultado.push_back(v1.at(i));
+	for(j; j < v2.size(); j++)
+		v_resultado.push_back(v2.at(j));
+
+	return v_resultado;
+}
+
+int main(int argc, char const *argv[]){
+
+	if (argc != 4){
+		cerr << "Formato " << argv[0] << "<ruta_del_archivo_de_entrada>  <num_elem>  <num_vect>" << endl;
+		return -1;
+    }
+
+	int tam_vectores = atoi(argv[2]); //Tamaño vectores
+	int num_vectores = atoi(argv[3]); //Número vectores
+	int num = 0;
+	string ruta=argv[1];
+	ifstream archivo(ruta.c_str());
+	vector<int> vector1;
+	vector<int> vector2;
+	vector<int> vectorRes;
+
+	//Rellenar los dos primeros vectores
+	for(int i = 0; i < 2*tam_vectores; i++){
+		archivo >> num;
+		if(i < tam_vectores)
+			vector1.push_back(num);
+		else
+			vector2.push_back(num);
+	}
+
+	vectorRes = combinarVectores(vector1, vector2);
+
+	for(int i = 0; i < num_vectores-2; i++){
+		vector1.clear();
+		for(int j = 0; j < tam_vectores; j++){
+			archivo >> num;
+			vector1.push_back(num);
+		}
+		vectorRes = combinarVectores(vector1, vectorRes);
+	}
+
+
+	for(int i = 0; i < vectorRes.size(); i++)
+		cout << vectorRes.at(i) << " ";
+
+	cout << endl;
+
+	return 0;
+}
