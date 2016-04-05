289f7f3f7e06a6ad14151fcc4b9e66bc945a1cd7
joserejon <jose.rejon.santiago@gmail.com>
2016-04-05 12:08:12 +0200

    FUNCIONA

--- Práctica2/RecursivoV1.cpp
+++ Práctica2/RecursivoV1.cpp
@@ -10,36 +10,37 @@
 using namespace std;
 
 
-class matrix
-{
-public:
-	matrix(int filas, int columnas)
-	{
-		datos.resize(filas);
-		for(int i = 0; i< filas; i++)
-		{
-			datos[i].resize(columnas);
-		}
-	}
-	vector< vector<int> > datos;
+class matrix{
 
-	/*
-     *
-     * @brief Función para sobrecargar el operados == para dos vectores
-     * @param vectores : matriz en que cada fila corresponde a un vector
-     *	 				 ordenado
-     * @return resultAux : vector ordenado a partir
-     */
-    matrix& operator=( const matrix  v2)
-    {
-        matrix& m= *this;
-        for (int i = 0; i < this->datos.size(); i++){
-            for (int j = 0; j< this->datos.at(0).size();j++){
-                this->datos[i][j] = v2.datos[i][j];
-            }
-        }
-        return *this;
-    }
+	public:
+		vector< vector<int> > datos;
+
+		matrix(int filas, int columnas){
+
+			datos.resize(filas);
+			for(int i = 0; i < filas; i++)
+				datos[i].resize(columnas);
+
+		}
+
+		/*
+	     *
+	     * @brief Función para sobrecargar el operados == para dos vectores
+	     * @param vectores : matriz en que cada fila corresponde a un vector
+	     *	 				 ordenado
+	     * @return resultAux : vector ordenado a partir
+	     */
+	    matrix& operator=(const matrix v2){
+
+	    	this->datos.resize(v2.datos.size());
+	    	this->datos.at(0).resize(v2.datos.at(0).size());
+
+	        for (int i = 0; i < v2.datos.size(); i++)
+	            for (int j = 0; j < v2.datos.at(i).size();j++)
+	                this->datos[i][j] = v2.datos[i][j];
+
+	        return *this;
+	    }
 
 };
 
@@ -57,8 +58,9 @@ void print(matrix v)
 		{
 			cout << v.datos[i][j] << " ";
 		}
+		cout << endl;
 	}
-	cout << endl;
+	
 }
 
 /*
@@ -69,53 +71,31 @@ void print(matrix v)
  * @param matrix v2
  * @return vector<int>
  */
-matrix merge( matrix v1,  matrix v2){
+matrix merge(const matrix v1, const matrix v2){
 
-	int f1 = 0,f2 = 0,c1 = 0,c2 = 0, fr=0, cr=0;
+	int f1 = 0,f2 = 0,c1 = 0,c2 = 0, fr=0, cr=0, tama_columnas = 0;
 
-	matrix v_resultado(v1.datos.size()+v2.datos.size(),v1.datos.at(0).size());
+	matrix v_resultado(1, v1.datos.at(0).size() + v2.datos.at(0).size());
 
 	//Meter números ordenados en el vector ordenado
-	while(f1 < v1.datos.size() && f2 < v2.datos.size() && c1 < v1.datos.at(0).size() && c2 < v2.datos.at(0).size()){
+	while(c1 < v1.datos.at(0).size() && c2 < v2.datos.at(0).size()){
 		if(v1.datos.at(f1).at(c1) < v2.datos.at(f2).at(c2)){
 			v_resultado.datos[fr][cr] = v1.datos.at(f1).at(c1);
 			c1++;
-			cr++;
 		}
 		else{
-			v_resultado.datos[fr][cr] = v1.datos.at(f2).at(c2);
+			v_resultado.datos[fr][cr] = v2.datos.at(f2).at(c2);
 			c2++;
-			cr++;
 		}
-		if (c1+1 % v1.datos.at(0).size()) f1++;
-		if (c2+1 % v1.datos.at(0).size()) f2++;
-		if (cr+1 % v1.datos.at(0).size()) fr++;
-
+		cr++;
 	}
 
-	//Terminar de completar vector resultado
-	for(f1; f1 < v1.datos.size(); f1++)
-	{
-		for (c1; c1 < v1.datos.at(0).size(); c1++)
-		{
-			v_resultado.datos[fr][cr] = v1.datos.at(f1).at(c1);
-			cr++;
-			if (cr+1 % v1.datos.at(0).size()) fr++;
-		}
+	//Terminar de introducir elementos
+	for(c1; c1 < v1.datos.at(0).size(); c1++, cr++)
+		v_resultado.datos[fr][cr] = v1.datos.at(0).at(c1);
 
-	}
-	for(f2; f2 < v2.datos.size(); f2++)
-	{
-		for (c2; c2 < v2.datos.at(0).size(); c2++)
-		{
-
-			v_resultado.datos[fr][cr] = v1.datos.at(f2).at(c2);
-			cr++;
-			if (cr+1 % v1.datos.at(0).size()) fr++;
-
-		}
-
-	}
+	for(c2; c2 < v2.datos.at(0).size(); c2++, cr++)
+		v_resultado.datos[fr][cr] = v2.datos.at(0).at(c2);
 
 	return v_resultado;
 }
@@ -128,59 +108,40 @@ matrix merge( matrix v1,  matrix v2){
  *	 				 ordenado
  * @return resultAux : vector ordenado a partir
  */
-matrix mergeKvectors(matrix &vectores)
-{
+matrix mergeKvectors(matrix &vectores){
 
-
+	//Caso base
     if (vectores.datos.size() <= 1)
-    {
-        cout<< "tamaño vetores.datos.size()" << endl;
-        cout<< vectores.datos.size() << endl;
         return vectores;
-    }
 
+    int middle = 0;
 
-    int middle;
+    //Calcular mitad de filas de la matriz
    	if(vectores.datos.size()%2 == 0)
-   	{
    		middle = vectores.datos.size()/2;
-   	}
    	else
-   	{
    		middle = (vectores.datos.size()/2) +1;
-    }
 
-
+   	//Dividir la matriz principal
     matrix up (middle ,vectores.datos.at(0).size());
 	matrix down (vectores.datos.size() - middle,vectores.datos.at(0).size());
 
-
-    matrix resultAux(100,100);
-    vector<int> Aux;
-
-
-
-    for (int i = 0; i < middle; i++){
-		for (int j = 0; j< vectores.datos.at(0).size();j++){
+	//Rellenar submatrices
+    for (int i = 0; i < middle; i++)
+		for (int j = 0; j < vectores.datos.at(i).size(); j++)
 			up.datos[i][j] = vectores.datos[i][j];
-		}
-    }
 
+    for (int i = middle; i < vectores.datos.size(); i++)
+		for (int j = 0; j < vectores.datos.at(i).size(); j++)
+			down.datos[i - middle][j] = vectores.datos[i][j];
 
-    for (int i = middle; i < vectores.datos.size(); i++){
-		for (int j = 0; j< vectores.datos.at(0).size();j++){
-			down.datos[i - middle][j]= vectores.datos[i][j];
-		}
-    }
-
-
-    cout << "HE LLEGADO!!!" << endl;
-    print(up);
-    print(down);
+	//Llamada recursiva para seguir subdividiendo matrices hasta el caso base
     up = mergeKvectors(up);
     down = mergeKvectors(down);
-    //resultAux = merge(up, down);
 
+    //Llamada a merge para combinar el resultado
+    matrix resultAux(1,1);
+    resultAux = merge(up, down);
 
 	return resultAux;
 }
@@ -214,13 +175,11 @@ int main(int argc, char const *argv[]){
 		}
 	}
 
-   vectorAux = mergeKvectors(vectorT);
+	cout << endl << endl << "MATRIZ ORIGINAL: " << endl;	
+	print(vectorT);
+	vectorAux = mergeKvectors(vectorT);
 
-	/*for (int i =0; i < vectorAux.size(); i++)
-		cout << vectorAux[i]<<" ";
-
-	cout << " " << endl;
-	*/
+	cout << endl << endl << "MATRIZ RESULTADO: " << endl;
 	print(vectorAux);
 	return 0;
 }
