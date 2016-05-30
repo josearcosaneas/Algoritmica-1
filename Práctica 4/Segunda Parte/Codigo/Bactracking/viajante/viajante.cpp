304c250aa4fd5c7b48dc1d0d1b9c5cffaf91e7ff
Miguepj <miguel.porcel92@gmail.com>
2016-05-30 22:53:08 +0200

    ya funciona todo (creo)

--- Práctica 4/Segunda Parte/Codigo/Bactracking/viajante/viajante.cpp
+++ Práctica 4/Segunda Parte/Codigo/Bactracking/viajante/viajante.cpp
@@ -72,31 +72,40 @@ vector< vector<double> > generaMatriz(vector <Ciudad> ciudades){
 	return salida;
 }
 
-void backtracking (int nivel, int ciudad_actual, vector<bool> &visitadas, int solucion[], const vector< vector<double> > &matriz) {
+void backtracking (int nivel, int ciudad_actual, vector<bool> &visitadas, int solucion[], const vector< vector<double> > &matriz
+	, double cota_local, int solucion_final[]) {
 
 	visitadas[ciudad_actual] = true;
 	solucion[nivel - 1] = ciudad_actual;
 	nodos_explorados++; 
 
+	//Recorremos todas las ciudades
 	for (int siguiente_ciudad = 0; siguiente_ciudad < visitadas.size(); siguiente_ciudad++) {
+		//Si la ciudad en la que estamos en este momento no ha sido visitada aún
 		if (visitadas[siguiente_ciudad] == false) {	
-
+			//Establecemos su cota local
 			cota_local = cota_local + matriz[ciudad_actual][siguiente_ciudad];
-			if (cota_local < cota_global) {				
-				backtracking(nivel + 1, siguiente_ciudad, visitadas, solucion, matriz);
+			//Comprobamos si esta solución va por buen camino
+			if (cota_local < cota_global) {	
+				//Descendemos un nivel en el árbol			
+				backtracking(nivel + 1, siguiente_ciudad, visitadas, solucion, matriz, cota_local, solucion_final);
 			}
-			else{
-				if (nivel == (visitadas.size() - 1)) {		
-
-					cota_local += matriz[0][ciudad_actual];					
-					if (cota_local < cota_global) {
-						cota_global = cota_local;
-					} 
-				}
-				else {
-					cota_local = cota_local - matriz[ciudad_actual][siguiente_ciudad];
-				}
-			}	
+			//Si estamos en un nodo hoja (solución)
+			if (nivel == (visitadas.size() - 1)) {		
+				//Cerramos el circuito (primera ciudad con última)
+				cota_local += matriz[0][siguiente_ciudad];			
+				//Si esta cota obtenida, es mejor que la solución que llevamos hasta el momento la actualizamos		
+				if (cota_local < cota_global) {
+					cota_global = cota_local;
+					for(int i = 0; i < visitadas.size(); i++)
+						solucion_final[i] = solucion[i];
+				} 
+			}
+			//En el caso de que no estemos en un nodo hoja, deshacemos los cambios que hemos hecho
+			else {
+				cota_local = cota_local - matriz[ciudad_actual][siguiente_ciudad];
+			}
+			
 			visitadas[siguiente_ciudad] = false;
 		}
 	}
@@ -125,8 +134,9 @@ int main (int argc, char* argv[]) {
 
 	vector<bool> visitadas(ciudades.size(), false);
 	int solucion[ciudades.size()];
+	int solucion_final[ciudades.size()];
 
-	backtracking (1, ciudades[0].indice-1, visitadas, solucion, matriz_distancias);
+	backtracking (1, ciudades[0].indice-1, visitadas, solucion, matriz_distancias, cota_local, solucion_final);
 
 	double distancia = 0;
 
@@ -140,13 +150,13 @@ int main (int argc, char* argv[]) {
 	cout << endl;
 
 	for(int i = 1; i < ciudades.size(); i++){
-		distancia += matriz_distancias[solucion[i-1]][solucion[i]];
-		cout << "CIUDAD " << solucion[i-1] + 1<< endl;
+		distancia += matriz_distancias[solucion_final[i-1]][solucion_final[i]];
+		cout << "CIUDAD " << solucion_final[i-1] + 1<< endl;
 	}
 
-	cout << "CIUDAD " << solucion[visitadas.size() - 1] + 1<< endl;
+	cout << "CIUDAD " << solucion_final[visitadas.size() - 1] + 1<< endl;
 
-	distancia += matriz_distancias[0][solucion[ciudades.size() - 1]];
+	distancia += matriz_distancias[0][solucion_final[ciudades.size() - 1]];
 
 	cout << "DISTANCIA: " << distancia << endl;
 
